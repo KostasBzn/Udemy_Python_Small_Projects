2f6451769ba3db951a4e6bbc6db14fc848f28a74
Kostas Bouzianis <kostasbouzianis@gmail.com>
2025-03-01 14:05:00 +0100

    functional

--- Birthday_Wisher_Automated_Email/main.py
+++ Birthday_Wisher_Automated_Email/main.py
@@ -1,16 +1,9 @@
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
 import smtplib
 import datetime as dt
 import csv
 import random
 
-sender = "example_email_sender.gmail.com"
+sender = "example_email_sender@gmail.com"
 app_password = "sender_app_password"
 subject = "Happy Birthday!"
 host = "smtp.gmail.com" # Differs for each email provider
@@ -23,7 +16,7 @@ def send_email(receiver, email_content):
         connection.sendmail(from_addr=sender, to_addrs=receiver,
                             msg=f"Subject:{subject}\n\n{email_content}")
         
-# unction to load random template and replace the receivers name
+# Function to load random template and replace the receivers name
 def random_template(name):
     letters = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
     random_template = random.choice(letters)
@@ -45,7 +38,9 @@ with open("./birthdays.csv", "r") as file:
     birthday_data = csv.reader(file)
     next(birthday_data)
     for row in birthday_data:
-        email = row[1]
+        receivers_email = row[1]
+        receivers_name = row[0]
+        email_content = random_template(receivers_name)
         if int(row[2]) == year and int(row[3]) == month and int(row[4]) == day:
-            send_email(email, "dd")
+            send_email(receivers_email, email_content)
 
